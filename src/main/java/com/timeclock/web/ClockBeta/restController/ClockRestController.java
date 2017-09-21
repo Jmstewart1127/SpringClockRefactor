9846f520e78f15e13344b4f11b24b126ab9adef4
Jacob Stewart <jmstewart1127@gmail.com>
2017-09-21 16:07:13 -0500

    new api's

--- src/main/java/com/timeclock/web/ClockBeta/restController/ClockRestController.java
+++ src/main/java/com/timeclock/web/ClockBeta/restController/ClockRestController.java
@@ -1,13 +1,8 @@
 package com.timeclock.web.ClockBeta.restController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.timeclock.web.ClockBeta.model.Clock;
@@ -28,27 +23,59 @@ public class ClockRestController {
 	}
 
 	/*
+	* Gets user by id
+	*/
+	@RequestMapping("/rest/get/employee/{id}")
+	public Clock getEmployee(@PathVariable int id) {
+		return clockService.findById(id);
+	}
+
+	/*
 	* Gets businessId that employee belongs to.
 	*/
-	@RequestMapping("/rest/get/business/by/user{id}")
+	@RequestMapping("/rest/get/business/by/user/{id}")
 	public int getBizIdByUserId(@PathVariable int id) {
 		return clockService.findBizIdById(id);
 	}
 
 	/*
-	 * Employee Clock In  
+ 	* Employee Clock In
+ 	*/
+	@RequestMapping(value="/rest/clock/in/{id}")
+	public void clockIn(@PathVariable int id) {
+		clockService.clockIn(id);
+	}
+
+	/*
+	* Employee Clock Out
+ 	*/
+	@RequestMapping(value="/rest/clock/out/{id}")
+	public void clockOut(@PathVariable int id) {
+		clockService.clockOut(id);
+	}
+
+	/*
+	 * Employee Clock In
+	 *
+	 * This method was the original way of clocking in and out.
+	 * However, during testing of the mobile app, it was found
+	 * that this method would not get the job done as the app
+	 * were intended.
+	 *
+	 * We wanted an employee's shift to end when they left the
+	 * premises, but with this method they would be clocked in
+	 * and out while on the premises.
 	 */
-	@RequestMapping(value="/rest/clockin/{id}")
-	public String clockIn(@PathVariable int id) {
-		int userId = id;
+	@RequestMapping(value="/rest/clock/in/current/status/{id}")
+	public String clockInByClockStatus(@PathVariable int id) {
 		
-		Boolean isClocked = clockService.findClockedById(userId);
+		Boolean isClocked = clockService.findClockedById(id);
 		
 		if (isClocked) {
-			clockService.clockOut(userId);
+			clockService.clockOut(id);
 			return "Shift End";
 		} else {
-			clockService.clockIn(userId);
+			clockService.clockIn(id);
 			return "Shift Start";
 		}
 	}
