8ab8a2cdbab4bb4b74f64285efe0435b8ccf94bc
Jmstewart1127 <Jmstewart1127@gmail.com>
2017-11-04 18:56:10 -0500

    cors

--- src/main/java/com/timeclock/web/ClockBeta/ClockBetaApplication.java
+++ src/main/java/com/timeclock/web/ClockBeta/ClockBetaApplication.java
@@ -2,10 +2,20 @@ package com.timeclock.web.ClockBeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Bean
+public WebMvcConfigurer corsConfigurer() {
+	return new WebMvcConfigurerAdapter() {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
+		}
+	};
+}
 
 @SpringBootApplication
 public class ClockBetaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClockBetaApplication.class, args);
 	}
