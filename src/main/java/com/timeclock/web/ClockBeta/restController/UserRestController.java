a37e7c9b4a8f12d992362d4d1c038eba03e34056
Jmstewart1127 <Jmstewart1127@gmail.com>
2017-11-15 18:29:11 -0600

    cors

--- src/main/java/com/timeclock/web/ClockBeta/restController/UserRestController.java
+++ src/main/java/com/timeclock/web/ClockBeta/restController/UserRestController.java
@@ -3,6 +3,7 @@ package com.timeclock.web.ClockBeta.restController;
 import com.timeclock.web.ClockBeta.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UserRestController {
 //        return auth.getName();
 //    }
     
+    @CrossOrigin(origins = {"http://localhost:3000", "https://spring-clock-ui.herokuapp.com"})
     @RequestMapping(value="/rest/login/{username}/{password}")
     public int loginTest(@PathVariable String username, @PathVariable String password, Authentication auth) {
         return userService.getIdByCredentials(username, password);
