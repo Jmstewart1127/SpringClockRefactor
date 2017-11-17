9ce4795623467592215bae10551be8619769c7ab
Jmstewart1127 <Jmstewart1127@gmail.com>
2017-11-17 16:17:04 -0600

    more cors, cors light

--- src/main/java/com/timeclock/web/ClockBeta/restController/BusinessRestController.java
+++ src/main/java/com/timeclock/web/ClockBeta/restController/BusinessRestController.java
@@ -4,6 +4,7 @@ import com.timeclock.web.ClockBeta.model.Business;
 import com.timeclock.web.ClockBeta.service.BusinessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class BusinessRestController {
     /*
     * Show businesses by user id
     */
+    @CrossOrigin(origins = {"http://localhost:3000", "https://spring-clock-ui.herokuapp.com"})
     @RequestMapping(value = "/rest/user/{id}/businesses")
     public Iterable<Business> getBusinessesByUserId(@PathVariable int id) {
         return businessService.findBusinessesByUserId(id);
@@ -25,6 +27,7 @@ public class BusinessRestController {
     /*
     * Show businesses by current logged in user
     */
+    @CrossOrigin(origins = {"http://localhost:3000", "https://spring-clock-ui.herokuapp.com"})
     @RequestMapping(value = "/rest/user/businesses")
     public Iterable<Business> getBusinessesByCurrentLogin(Authentication auth) {
         System.out.println(":: " + businessService.findByCurrentUserId(auth));
